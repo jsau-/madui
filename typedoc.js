445d0ae392a8a6bfc7b15dd24716d5a0c9312087
Jon Saunders <jon@jsau.co.uk>
2020-09-25 11:42:29 +0100

    Add typedoc config

---
+++ typedoc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  out: './docs/',
+  includes: './src',
+  exclude: [
+    'src/**/*.unit.test.ts',
+    'src/**/*.unit.test.tsx',
+    'src/**/*.stories.ts',
+    'src/**/*.stories.tsx',
+  ]
+};
