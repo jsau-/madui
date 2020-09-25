3812df623d6f03a00d1297262e0eedd202c69745
Jon Saunders <jon@jsau.co.uk>
2020-09-25 12:19:17 +0100

    Add example text component

---
+++ src/Text/Text.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+/**
+ * Available text variant options.
+ */
+type TextVariant = 'heading' | 'subheading' | 'body' | 'caption';
+
+type TextVariantMap<T> = { [variant in TextVariant]: T };
+
+/**
+ * Object mapping possible TextVariants to HTML element tags.
+ */
+const textVariantToWrapper: TextVariantMap<keyof JSX.IntrinsicElements> = {
+  body: 'p',
+  caption: 'h6',
+  heading: 'h1',
+  subheading: 'h3',
+};
+
+export interface TextProps {
+  children?: React.ReactNode;
+  variant?: TextVariant;
+}
+
+export const Text: React.FunctionComponent<TextProps> = (props: TextProps) => {
+  const WrapperNode = props.variant ? textVariantToWrapper[props.variant] : 'p';
+  return <WrapperNode>{props.children}</WrapperNode>;
+};
