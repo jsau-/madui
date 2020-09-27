d539cae3e610a7980593453d4ae5fcebc8190e21
Jon Saunders <jon@jsau.co.uk>
2020-09-27 17:58:57 +0100

    Fix border styles for divider

--- src/Divider/Divider.styles.ts
+++ src/Divider/Divider.styles.ts
@@ -4,9 +4,9 @@ import { Theme } from '../types/Theme';
 export const useStyles = makeStyles(
   (theme: Theme) => ({
     root: {
-      borderStyle: 'solid',
-      borderWidth: 1,
-      color: theme.palette.divider,
+      backgroundColor: theme.palette.divider,
+      border: `1px solid ${theme.palette.divider}`,
+      borderRadius: theme.border.radius,
       marginBottom: theme.spacing.unit,
       marginTop: theme.spacing.unit,
     },
