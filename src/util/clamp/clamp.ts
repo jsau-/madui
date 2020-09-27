4117d922bbd7b1d9e8f9e3a1675dec493d368eda
Jon Saunders <jon@jsau.co.uk>
2020-09-27 17:21:02 +0100

    Add clamp util function

---
+++ src/util/clamp/clamp.ts
@@ -0,0 +1,18 @@
+/**
+ * Clamp a number between two values.
+ *
+ * @param value Value to clamp.
+ * @param min Minimum possible value.
+ * @param max Maximum possible value.
+ */
+export const clamp = (value: number, min: number, max: number): number => {
+  if (value > max) {
+    return max;
+  }
+
+  if (value < min) {
+    return min;
+  }
+
+  return value;
+};
