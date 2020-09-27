0a2632a9c872291ee64767a0f74e80d883b22cda
Jon Saunders <jon@jsau.co.uk>
2020-09-27 14:26:05 +0100

    Add fileoverview to util/theming

--- src/util/theming/theming.ts
+++ src/util/theming/theming.ts
@@ -1,3 +1,10 @@
+/**
+ * @fileoverview Custom namespaced react-jss context, to allow multiple
+ * libraries using react-jss to work side-by-side in a single application.
+ *
+ * @see https://cssinjs.org/react-jss/?v=v10.4.0#using-custom-theming-context
+ */
+
 import React from 'react';
 import { createTheming } from 'react-jss';
 import { createTheme } from '../../createTheme';
