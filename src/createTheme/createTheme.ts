f347d0852e67fc9d729eba87bfcc50dce76c6029
Jon Saunders <jon@jsau.co.uk>
2020-09-27 14:13:13 +0100

    Add docs

--- src/createTheme/createTheme.ts
+++ src/createTheme/createTheme.ts
@@ -3,6 +3,14 @@ import { defaultTheme } from '../defaultTheme';
 import { RecursivePartial } from '../types/RecursivePartial';
 import { Theme } from '../types/Theme';
 
+/**
+ * Create a theme object for use in <ThemeProvider />. Note that any required
+ * theme fields that aren't provided will be taken from the default theme.
+ *
+ * @param theme Custom user theme.
+ * @returns Theme object, with any un-specified values taken from the default
+ * theme.
+ */
 export const createTheme = <T extends RecursivePartial<Theme>>(
   theme?: T,
 ): T & Theme => defaultsDeep({}, theme, defaultTheme);
--- src/makeStyles/makeStyles.ts
+++ src/makeStyles/makeStyles.ts
@@ -5,6 +5,15 @@ import { RecursivePartial } from '../types/RecursivePartial';
 import { Theme } from '../types/Theme';
 import { defaultsDeep } from 'lodash';
 
+/**
+ * Make a function which, when invoked, returns an object of component styles.
+ * Note that this function will also apply override styles from the theme.
+ *
+ * @param styles - Function generating component styles object from the theme.
+ * @param component - Component name, used for fetching override styles from the
+ * theme.
+ * @returns Hook function for getting styles object.
+ */
 export const makeStyles = <C extends string = string>(
   styles: ((theme: RecursivePartial<Theme> & Theme) => Styles<C>),
   component?: string,
