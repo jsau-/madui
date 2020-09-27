d01ebc57e8e492b90d3d93959d45d7adc80b6e1c
Jon Saunders <jon@jsau.co.uk>
2020-09-27 22:42:50 +0100

    Increase height of <LinearProgress />

--- src/LinearProgress/LinearProgress.styles.ts
+++ src/LinearProgress/LinearProgress.styles.ts
@@ -21,7 +21,7 @@ export const useStyles = makeStyles(
     },
     root: {
       borderRadius: 4,
-      height: 2,
+      height: 4,
       overflow: 'hidden',
       position: 'relative',
     },
