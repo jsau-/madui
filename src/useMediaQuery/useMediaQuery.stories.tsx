6cfe8792578975f355039eecede4f080d0dc75ef
Jon Saunders <jon@jsau.co.uk>
2020-09-29 01:02:44 +0100

    Use <Text /> in useMediaQuery stories

--- src/useMediaQuery/useMediaQuery.stories.tsx
+++ src/useMediaQuery/useMediaQuery.stories.tsx
@@ -4,6 +4,7 @@ import { useMediaQuery } from '.';
 import { aboveWidth } from '../styles/breakpoint/aboveWidth';
 import { belowWidth } from '../styles/breakpoint/belowWidth';
 import { betweenWidth } from '../styles/breakpoint/betweenWidth';
+import { Text } from '../Text';
 
 export default {
   title: 'Hooks/Layout/useMediaQuery',
@@ -12,19 +13,19 @@ export default {
 const SmallComponent: React.FunctionComponent = () => {
   const isSmall = useMediaQuery(belowWidth(800));
   const content = isSmall ? 'Less than 800px' : 'More than 800px';
-  return <p>{content}</p>;
+  return <Text>{content}</Text>;
 }
 
 const MediumComponent = () => {
   const isMedium = useMediaQuery(betweenWidth(800, 1600));
   const content = isMedium ? 'Between 800-1600px' : 'Not between 800-1600px';
-  return <p>{content}</p>;
+  return <Text>{content}</Text>;
 }
 
 const LargeComponent = () => {
   const isLarge = useMediaQuery(aboveWidth(1600));
   const content = isLarge ? 'Above 1600px' : 'Not above 1600px';
-  return <p>{content}</p>;
+  return <Text>{content}</Text>;
 }
 
 const SmallTemplate: Story = () => <SmallComponent />;
