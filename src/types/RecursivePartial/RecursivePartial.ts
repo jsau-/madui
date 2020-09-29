6fc15368d15a392ee97068c2fe4dbb667ae9876e
Jon Saunders <jon@jsau.co.uk>
2020-09-29 01:31:43 +0100

    Add docs, use strong type for breakpoints in theme

--- src/types/RecursivePartial/RecursivePartial.ts
+++ src/types/RecursivePartial/RecursivePartial.ts
@@ -1,3 +1,8 @@
+/**
+ * Recursively re-define an existing type s.t. every field within it is
+ * optional. Useful when providing an optional subset of a known type which is
+ * merged with defaults, eg. when creating themes.
+ */
 export type RecursivePartial<T> = {
   [P in keyof T]?: RecursivePartial<T[P]>;
 };
--- src/types/Theme/Theme.ts
+++ src/types/Theme/Theme.ts
@@ -1,26 +1,40 @@
+import { Breakpoint } from '../Breakpoint';
 import { Elevation } from '../Elevation';
 
 interface ThemeRecord {
-  [key: string]: string | number | ThemeRecord | Record<string, ThemeRecord>;
+  [key: string]:
+    | string
+    | number
+    | boolean
+    | bigint
+    | ThemeRecord
+    | Record<string, ThemeRecord>;
 }
+
+type Breakpoints = { [breakpoint in Breakpoint]: number } & ThemeRecord;
 
 interface PaletteColor extends ThemeRecord {
   contrast: string;
   main: string;
 }
 
+/**
+ * Themes are used to style components within the library.
+ *
+ * Note that only defined primitives are allowed as theme fields. The intent is
+ * to ensure that themes are kept simple, pure, and serializable to allow
+ * transfer (eg. loading a user theme over HTTP).
+ *
+ * All components defined in the library can be overriden by setting styles in
+ * the overrides section of the theme. See individual component style files for
+ * classes to target. Overrides are merged with higher priority than base
+ * styles.
+ */
 export interface Theme extends ThemeRecord {
   border: {
     radius: number;
   };
-  breakpoints: {
-    xs: number;
-    sm: number;
-    md: number;
-    lg: number;
-    xl: number;
-    xxl: number;
-  };
+  breakpoints: Breakpoints;
   overrides: Record<string, ThemeRecord>;
   palette: {
     background: string;
