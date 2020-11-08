3ac3859ddfefb3b8734441a01d9221b4a98e6171
Jon Saunders <jon@jsau.co.uk>
2020-11-08 12:38:25 +0000

    Drop Hungarian notation

--- src/useHoverWithAim/useHoverWithAim.stories.tsx
+++ src/useHoverWithAim/useHoverWithAim.stories.tsx
@@ -6,12 +6,12 @@ const TestComponent = () => {
   const refSource = useRef<HTMLDivElement>(null);
   const refTarget = useRef<HTMLDivElement>(null);
 
-  const bHoveringWithAim = useHoverWithAim(refSource, refTarget);
+  const isHoveringWithAim = useHoverWithAim(refSource, refTarget);
 
   return (
-    <div style={{ background: bHoveringWithAim ? 'green': 'transparent', transition: 'all 0.5s ease', position: 'relative', width: '100vw', height: '100vh' }}>
+    <div style={{ background: isHoveringWithAim ? 'green': 'transparent', transition: 'all 0.5s ease', position: 'relative', width: '100vw', height: '100vh' }}>
       <div id="source" ref={refSource} style={{ background: 'red', transition: 'all 0.5s ease', position: 'absolute', top: 0, height: 50, left: 300, width: 50 }} />
-      <div id="target" ref={refTarget} style={{ background: 'orange', transition: 'all 0.5s ease', position: 'absolute', top: 50, height: 250, width: 150, left: bHoveringWithAim ?  200 : 400 }} />
+      <div id="target" ref={refTarget} style={{ background: 'orange', transition: 'all 0.5s ease', position: 'absolute', top: 50, height: 250, width: 150, left: isHoveringWithAim ?  200 : 400 }} />
     </div>
   )
 };
