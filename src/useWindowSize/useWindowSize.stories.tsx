029f06b3c4b6eadf4840f59fd331bee42ef67684
Jon Saunders <jon@jsau.co.uk>
2020-11-08 12:47:29 +0000

    Add useWindowSize stories

---
+++ src/useWindowSize/useWindowSize.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { useWindowSize } from '.';
+import { Text } from '../Text';
+
+export default {
+  title: 'Hooks/Layout/useWindowSize',
+} as Meta;
+
+const TestComponent: React.FunctionComponent = () => {
+  const mousePosition = useWindowSize();
+  return <Text>{JSON.stringify(mousePosition)}</Text>;
+}
+
+const Template: Story = () => <TestComponent />;
+
+export const Default = Template.bind({});
